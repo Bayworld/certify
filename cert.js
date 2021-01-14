3175711fc4de3f0d375338eabcfc1ad88f874b49
Bayworld <abayomikiel@gmail.com>
2021-01-14 11:57:18 +0100

    'learn'

--- cert.js
+++
@@ -1,9 +0,0 @@
-const rainbow =
-['gray','orange','cream','green','blue','rebeccapurple','violet', 'gold'];
-setInterval(
-function change() {
-document.body.style.background =
- rainbow[Math.floor(7*Math.random())];
-},2000);
-
-
